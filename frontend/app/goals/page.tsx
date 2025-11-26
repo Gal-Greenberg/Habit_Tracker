ae8274ba07fa36cd9d9ff8631b19307d498ee0db
Gal Greenberg <39677930+Gal-Greenberg@users.noreply.github.com>
2025-11-26 07:17:31 +0200

    display goals and habits

--- frontend/app/goals/page.tsx
+++ frontend/app/goals/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { getGoals } from 'services/goals';
 
 const Goals = () => {
-    const [goals, setGoals] = useState([]);
+    const [goals, setGoals] = useState<any[]>([]);
     const router = useRouter();
 
     useEffect(() => {
@@ -12,15 +13,24 @@ const Goals = () => {
             router.push('/');
             return;
         }
+        getGoals().then(fetchedGoals => {
+            setGoals(fetchedGoals);
+            // console.log(fetchedGoals);
+        });
     }, []);
 
     return (
        <div className="w-full h-full relative">
-            <div className="mt-6 text-xl text-textMain">
-                {goals.length === 0 && <p>No golas found. Start by creating a new goal!</p>}
-            </div>
+            {goals.length === 0 && <p className="mt-6 text-xl text-textMain">No golas found. Start by creating a new goal!</p>}
 
-            <button className="absolute right-6 bottom-10 bg-bgButton text-white px-4 py-2 rounded hover:bg-bgButtonDark"
+            {goals.map((goal) => (
+                <div key={goal._id} className="p-4 mb-4 border border-gray-300 rounded-lg shadow-sm">
+                    <h2 className="text-lg font-semibold mb-2">{goal.title}</h2>
+                    <p className="text-gray-600">{goal.description}</p>
+                </div>
+            ))}
+
+            <button className="absolute right-6 mb-20 bg-bgButton text-white px-4 py-2 rounded hover:bg-bgButtonDark"
                 onClick={() => router.push('/goals/create')}>
                 Create New Goal
             </button>
--- frontend/app/habits/page.tsx
+++ frontend/app/habits/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { getHabits } from 'services/habits';
 
 const Habits = () => {
-    const [habits, setHabits] = useState([]);
+    const [habits, setHabits] = useState<any[]>([]);
     const router = useRouter();
 
     useEffect(() => {
@@ -12,15 +13,24 @@ const Habits = () => {
             router.push('/');
             return;
         }
+        getHabits().then(fetchedHabits => {
+            setHabits(fetchedHabits);
+            // console.log(fetchedHabits);
+        });
     }, []);
 
     return (
         <div className="w-full h-full relative">
-            <div className="mt-6 text-xl text-textMain">
-                {habits.length === 0 && <p>No habits found. Start by creating a new habit!</p>}
-            </div>
+            {habits.length === 0 && <p className="mt-6 text-xl text-textMain">No habits found. Start by creating a new habit!</p>}
 
-            <button className="absolute right-6 bottom-10 bg-bgButton text-white px-4 py-2 rounded hover:bg-bgButtonDark"
+            {habits.map((habit) => (
+                <div key={habit._id} className="p-4 mb-4 border border-gray-300 rounded-lg shadow-sm">
+                    <h2 className="text-lg font-semibold mb-2">{habit.title}</h2>
+                    <p className="text-gray-600">{habit.description}</p>
+                </div>
+            ))}
+
+            <button className="absolute right-6 bg-bgButton text-white px-4 py-2 rounded hover:bg-bgButtonDark"
                 onClick={() => router.push('/habits/create')}>
                 Create New Habit
             </button>
